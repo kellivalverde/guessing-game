3087f97d36abc01a7c598af9e2103a5ba7ee7c02
kellivalverde <paughka@gmail.comls>
2020-01-04 16:56:11 -0500

    while loop

--- src/game/GuessingGame.java
+++ src/game/GuessingGame.java
@@ -8,23 +8,34 @@ public class GuessingGame {
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Please guess a number between 1 and 10:");
+		System.out.println("Please guess a number between 1 and 10:");
 		
 		int numGuess = input.nextInt();  // holds the info from the user
 		
+		int guessAttempts = 0;
+		
+		while(guessAttempts < 3)  //condition  - can be tested for true "while this is true"
+		
 		if(numGuess == 7){
 			System.out.println("Yay! That's correct! You win!");
+			break;
 		}
 		else if(numGuess == 0) {
-			System.out.print("Please guess a number between 1 and 10:");
-		}
+			System.out.println("Sorry, that's no good. Please guess a number between 1 and 10:");
+			numGuess = input.nextInt();
+		}	
+		else if(numGuess == -1) {
+			System.exit(numGuess);
+		}	
 		
 		else {
 			System.out.println("Sorry, you lost.");
-		}
+			System.out.println("try again");
+			guessAttempts += 1;
+			
+			 }
 	
 		
 		input.close();
 	}
-
 }
