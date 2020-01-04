24b0d89c8d6963d3e2e5d5f531fe9629d44bff71
kellivalverde <paughka@gmail.comls>
2020-01-04 16:06:45 -0500

    redoing 1st push

--- src/game/GuessingGame.java
+++ src/game/GuessingGame.java
@@ -15,6 +15,10 @@ public class GuessingGame {
 		if(numGuess == 7){
 			System.out.println("Yay! That's correct! You win!");
 		}
+		else if(numGuess == 0) {
+			System.out.print("Please guess a number between 1 and 10:");
+		}
+		
 		else {
 			System.out.println("Sorry, you lost.");
 		}
