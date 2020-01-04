7cb4be32f82de31f0bcc0068ea601074959b1f69
kellivalverde <paughka@gmail.comls>
2020-01-04 15:52:21 -0500

    first commit

---
+++ src/game/GuessingGame.java
@@ -0,0 +1,26 @@
+package game;
+
+import java.util.Scanner;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please guess a number between 1 and 10:");
+		
+		int numGuess = input.nextInt();  // holds the info from the user
+		
+		if(numGuess == 7){
+			System.out.println("Yay! That's correct! You win!");
+		}
+		else {
+			System.out.println("Sorry, you lost.");
+		}
+	
+		
+		input.close();
+	}
+
+}
