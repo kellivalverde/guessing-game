86f168b3433f398b90d46417b26e4f6f082af4d1
kellivalverde <paughka@gmail.comls>
2020-01-05 10:12:55 -0500

    updated some formatting

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,31 +1,28 @@
-package game;
-
 import java.util.Scanner;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GuessingGame {
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);  //input
 
 		System.out.println("Please guess a number between 1 and 10:");
 
 		
 		int numGuess = input.nextInt(); // holds the info from the user
+		int guessAttempts = 0;
+		int secretNum = ThreadLocalRandom.current().nextInt(1, 11); // did 11 instead of 10 --> inclusive of 10
+
+		// would show me the targetNum so I know it's working:
+		// System.out.println(secretNum);
+		 
 
 		// loop
-
-		int guessAttempts = 0;
-		int targetNum = ThreadLocalRandom.current().nextInt(1, 11); // did 11 instead of 10 --> inclusive of 10
-
-		/*
-		 * would show me the targetNum so I know it's working:
-		 * System.out.println(targetNum);
-		 */
-
+		
 		do {
-			if (numGuess == targetNum) {
+			if (numGuess == secretNum) {
 				System.out.println("Yay! That's correct! You win!");
 				break;
 			} else if (numGuess == 0) {
@@ -33,10 +30,10 @@ public class GuessingGame {
 				numGuess = input.nextInt();
 			} else if (numGuess == -1) {
 				System.exit(numGuess); // kills the program
-			} else if (numGuess < targetNum) {
+			} else if (numGuess < secretNum) {
 				System.out.println("Try a higher number");
 				numGuess = input.nextInt();
-			} else if (numGuess > targetNum) {
+			} else if (numGuess > secretNum) {
 				System.out.println("Try a lower number");
 				numGuess = input.nextInt();
 			}
@@ -44,7 +41,7 @@ public class GuessingGame {
 
 		} while (guessAttempts < 3); // condition - can be tested for true "while this is true"
 
-		System.out.println("Done.");
+		System.out.println("No more attempts. You are done.");
 
 		input.close();
 	}
