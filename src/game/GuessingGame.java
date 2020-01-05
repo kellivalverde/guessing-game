ea5760fd0603518c67ac27914dfa239871b05b8f
kellivalverde <paughka@gmail.comls>
2020-01-04 19:01:11 -0500

    new do while loop added

--- src/game/GuessingGame.java
+++ src/game/GuessingGame.java
@@ -12,29 +12,35 @@ public class GuessingGame {
 		
 		int numGuess = input.nextInt();  // holds the info from the user
 		
+		// loop
+		
 		int guessAttempts = 0;
 		
-		while(guessAttempts < 3)  //condition  - can be tested for true "while this is true"
-		
-		if(numGuess == 7){
-			System.out.println("Yay! That's correct! You win!");
-			break;
-		}
-		else if(numGuess == 0) {
-			System.out.println("Sorry, that's no good. Please guess a number between 1 and 10:");
-			numGuess = input.nextInt();
-		}	
-		else if(numGuess == -1) {
-			System.exit(numGuess);
-		}	
-		
-		else {
-			System.out.println("Sorry, you lost.");
-			System.out.println("try again");
-			guessAttempts += 1;
-			
-			 }
+		do {
+			if(numGuess == 7){
+				System.out.println("Yay! That's correct! You win!");
+				break;
+			}
+			else if(numGuess == 0) {
+				System.out.println("Sorry, that's no good. Please guess a number between 1 and 10:");
+				numGuess = input.nextInt();
+			}	
+			else if(numGuess == -1) {
+				System.exit(numGuess);   // kills the program
+			}	
+			else if(numGuess < 7){
+				System.out.println("Try a higher number");
+				numGuess = input.nextInt();
+			}
+			else if(numGuess > 7){
+				System.out.println("Try a lower number");
+				numGuess = input.nextInt();
+			}
+				guessAttempts += 1;
 	
+		} while(guessAttempts < 3);      //condition  - can be tested for true "while this is true"
+	
+		System.out.println("Done.");
 		
 		input.close();
 	}
